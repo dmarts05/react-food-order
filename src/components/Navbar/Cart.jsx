046f9b59de1d157b9a09e9c1d7d2d000fa3397b4
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-02-02 00:38:00 +0100

    Improve responsive layout of navbar

--- src/components/Navbar/Cart.jsx
+++ src/components/Navbar/Cart.jsx
@@ -2,7 +2,7 @@ import { FaShoppingCart } from 'react-icons/fa';
 
 export default function Cart() {
   return (
-    <div className='flex cursor-pointer flex-wrap content-center justify-center gap-4 rounded-full bg-red-900 px-4 py-2'>
+    <div className='flex cursor-pointer select-none place-items-center justify-center gap-4 rounded-full bg-red-900 px-4 py-2'>
       <FaShoppingCart className='text-lg' />
       <span className='font-semibold'>Your Cart</span>
       <div className='w-6 rounded-full bg-red-700 text-center font-semibold'>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,8 @@ import Cart from './Cart';
 
 export default function Navbar() {
   return (
-    <nav className='flex h-20 flex-wrap content-center justify-between bg-red-700 px-4 shadow-md sm:px-8'>
-      <h1 className='self-center text-2xl font-semibold sm:text-3xl'>
+    <nav className='flex h-20 flex-wrap place-items-center justify-between bg-red-700 px-4 shadow-md sm:px-8'>
+      <h1 className='select-none text-2xl font-semibold sm:text-3xl'>
         ReactMeals
       </h1>
       <Cart />
