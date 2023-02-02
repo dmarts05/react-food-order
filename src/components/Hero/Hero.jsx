17e8b9200d20a3f8c94c26a2e443af0702bb1d14
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-02-02 18:22:41 +0100

    Add hero text

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,29 @@
+import Card from '../UI/Card';
 import foodImg from '../../assets/food.webp';
 
 export default function Hero() {
   return (
-    <header>
-      <div className='pointer-events-none absolute top-[13rem] -z-10 h-0 w-0 border-b-[8rem] border-l-[100vw] border-transparent border-b-zinc-800' />
+    <header className='flex items-center justify-center'>
       <img
         src={foodImg}
-        className='pointer-events-none absolute -z-20 h-64 w-full select-none object-cover brightness-90 filter'
+        className='pointer-events-none h-96 w-full select-none object-cover brightness-90 filter'
       />
-      Delicious Food, Delivered To You
+      <Card
+        className='absolute flex w-2/3 flex-col gap-3 text-center'
+        dark={true}
+      >
+        <h2 className='text-2xl font-semibold'>
+          Delicious Food, Delivered To You
+        </h2>
+        <p>
+          Choose your favorite meal from our broad selection of available meals
+          and enjoy a delicious lunch or dinner at home
+        </p>
+        <p>
+          All our meals are cooked with igh-quality ingredients, just-in.time
+          and of course by experienced chefs!
+        </p>
+      </Card>
     </header>
   );
 }
--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,3 +1,11 @@
-export default function Card() {
-  return <div>Card</div>;
+export default function Card(props) {
+  return (
+    <div
+      className={`rounded-xl ${
+        props.dark ? 'bg-zinc-800' : 'bg-white'
+      } p-6 shadow-lg ${props.className}`}
+    >
+      {props.children}
+    </div>
+  );
 }
