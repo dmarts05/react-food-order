bb74720337902a424154e244a3e5bcccdc705e38
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-02-02 01:35:25 +0100

    Fix z-index of diagonal

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,10 +3,10 @@ import foodImg from '../../assets/food.webp';
 export default function Hero() {
   return (
     <header>
-      <div className='absolute top-[13rem] h-0 w-0 border-b-[8rem] border-l-[100vw] border-transparent border-b-zinc-800' />
+      <div className='pointer-events-none absolute top-[13rem] -z-10 h-0 w-0 border-b-[8rem] border-l-[100vw] border-transparent border-b-zinc-800' />
       <img
         src={foodImg}
-        className='absolute -z-10 h-64 w-full object-cover brightness-90 filter'
+        className='pointer-events-none absolute -z-20 h-64 w-full select-none object-cover brightness-90 filter'
       />
       Delicious Food, Delivered To You
     </header>
